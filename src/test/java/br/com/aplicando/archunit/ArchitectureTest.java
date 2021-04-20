031b2eed55e68b09ccf71b3f5869f7488b44010e
Gabriel Augusto <biga00145@gmail.com>
2021-04-19 22:10:46 -0300

    Definindo o nome das classes

--- src/test/java/br/com/aplicando/archunit/ArchitectureTest.java
+++ src/test/java/br/com/aplicando/archunit/ArchitectureTest.java
@@ -5,6 +5,8 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
@@ -14,10 +16,27 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 )
 public class ArchitectureTest {
 
+    // Definindo Nomes das Classes
+    
     @ArchTest
     static ArchRule ClassesQuePossuemAnotacaoController_DeveraoFinalizarComNomeController =
             classes()
-                    .that().resideInAPackage("..controller..")
-                    .or().areAnnotatedWith(Controller.class)
-                    .should().haveSimpleNameEndingWith("Controller");
+                    .that().areAnnotatedWith(Controller.class)
+                    .should().haveSimpleNameEndingWith("Controller")
+            .as("Caro desenvolvedor, todas as nossas classes que estão anotadas como Controller, deverão ter o nome finalizado com Controller");
+
+    @ArchTest
+    static ArchRule ClassesQuePossuemAnotacaoRepository_DeveraoFinalizarComNomeController =
+            classes()
+                    .that().areAnnotatedWith(Repository.class)
+                    .should().haveSimpleNameEndingWith("Repository")
+                    .as("Caro desenvolvedor, todas as nossas classes que estão anotadas como Repository, deverão ter o nome finalizado com Repository");
+
+    @ArchTest
+    static ArchRule ClassesQuePossuemAnotacaoService_DeveraoFinalizarComNomeController =
+            classes()
+                    .that().areAnnotatedWith(Service.class)
+                    .should().haveSimpleNameEndingWith("Service")
+                    .as("Caro desenvolvedor, todas as nossas classes que estão anotadas como Service, deverão ter o nome finalizado com Service");
+
 }
